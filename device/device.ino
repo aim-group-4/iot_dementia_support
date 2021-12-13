582ffcdf640a6e6394f6ef4eff1b61229a063f20
davdleet <davdleet1@gmail.com>
2021-12-13 23:40:39 +0900

    log function with time stamp

--- device/device.ino
+++ device/device.ino
@@ -1,81 +1,17 @@
-/*
-  Simple WebSocket client for ArduinoHttpClient library
-  Connects to the WebSocket server, and sends a hello
-  message every 5 seconds
-
-  created 28 Jun 2016
-  by Sandeep Mistry
-  modified 22 Jan 2019
-  by Tom Igoe
-
-  this example is in the public domain
-*/
-#include <ArduinoHttpClient.h>
-#include <WiFi101.h>
-#define SECRET_SSID ""
-#define SECRET_PASS ""
-
-///////please enter your sensitive data in the Secret tab/arduino_secrets.h
-/////// Wifi Settings ///////
-char ssid[] = SECRET_SSID;
-char pass[] = SECRET_PASS;
-
-char serverAddress[] = "echo.websocket.org";  // server address
-int port = 80;
-
-WiFiClient wifi;
-WebSocketClient client = WebSocketClient(wifi, serverAddress, port);
-int status = WL_IDLE_STATUS;
-int count = 0;
-
+#include <SoftwareSerial.h>
+ 
+SoftwareSerial espSerial(2, 3); // RX, TX
+ 
 void setup() {
   Serial.begin(9600);
-  while ( status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to Network named: ");
-    Serial.println(ssid);                   // print the network name (SSID);
-
-    // Connect to WPA/WPA2 network:
-    status = WiFi.begin(ssid, pass);
-  }
-
-  // print the SSID of the network you're attached to:
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
-
-  // print your WiFi shield's IP address:
-  IPAddress ip = WiFi.localIP();
-  Serial.print("IP Address: ");
-  Serial.println(ip);
+  espSerial.begin(9600);
 }
-
-void loop() {
-  Serial.println("starting WebSocket client");
-  client.begin();
-
-  while (client.connected()) {
-    Serial.print("Sending hello ");
-    Serial.println(count);
-
-    // send a hello #
-    client.beginMessage(TYPE_TEXT);
-    client.print("hello ");
-    client.print(count);
-    client.endMessage();
-
-    // increment count for next message
-    count++;
-
-    // check if a message is available to be received
-    int messageSize = client.parseMessage();
-
-    if (messageSize > 0) {
-      Serial.println("Received a message:");
-      Serial.println(client.readString());
-    }
-
-    // wait 5 seconds
-    delay(5000);
+ 
+void loop() { // run over and over
+  if (espSerial.available()) {
+    Serial.write(espSerial.read());
   }
-
-  Serial.println("disconnected");
+  if (Serial.available()) {
+    espSerial.write(Serial.read());
+  }
 }
