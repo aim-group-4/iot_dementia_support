ed42ca40467f00ec11a0168b11e58ebbcf731e46
davdleet <davdleet1@gmail.com>
2021-12-15 21:41:01 +0900

    arduino code template

--- device/device.ino
+++ device/device.ino
@@ -1,17 +1,48 @@
-#include <SoftwareSerial.h>
- 
-SoftwareSerial espSerial(2, 3); // RX, TX
- 
-void setup() {
-  Serial.begin(9600);
-  espSerial.begin(9600);
+#include <Arduino.h>
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+
+#include <SocketIoClient.h>
+
+#define USE_SERIAL Serial
+
+ESP8266WiFiMulti WiFiMulti;
+SocketIoClient webSocket;
+
+void event(const char * payload, size_t length) {
+  USE_SERIAL.printf("got message: %s\n", payload);
 }
- 
-void loop() { // run over and over
-  if (espSerial.available()) {
-    Serial.write(espSerial.read());
-  }
-  if (Serial.available()) {
-    espSerial.write(Serial.read());
-  }
+
+void setup() {
+    USE_SERIAL.begin(115200);
+
+    USE_SERIAL.setDebugOutput(true);
+
+    USE_SERIAL.println();
+    USE_SERIAL.println();
+    USE_SERIAL.println();
+
+    for(uint8_t t = 4; t > 0; t--) {
+        USE_SERIAL.printf("[SETUP] BOOT WAIT %d...\n", t);
+        USE_SERIAL.flush();
+        delay(1000);
+    }
+
+    
+    WiFiMulti.addAP("ssid", "pw");
+
+    while(WiFiMulti.run() != WL_CONNECTED) {
+        delay(100);
+    }
+
+    webSocket.on("event", event);
+    webSocket.begin("https://dry-garden-16739.herokuapp.com/", 9000, "/socket.io/?transport=websocket");
+    // use HTTP Basic Authorization this is optional remove if not needed
+    //webSocket.setAuthorization("username", "password");
+}
+
+void loop() {
+    webSocket.emit("alert_client", "1"); 
+    webSocket.loop();
 }
